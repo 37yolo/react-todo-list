472afa9ae1b6b2c90cc33fa00503f5e9b7596a33
37yolo <leon.vvvvvvv@gmail.com>
2024-08-06 10:44:41 +0700

    Revert "fix jittering button"
    This reverts commit 42004738871cfeab7a1504ac8ad504e341bbb98c.

--- src/component/button/button.jsx
+++ src/component/button/button.jsx
@@ -2,9 +2,6 @@ import './style.css'
 export default function Button({type , text, className, onClick}) {
 
     return<>
-    <div className='paddingButton'>
-        <button onClick={onClick} type={type} className={className?className:'btn'} role="button">{text}</button>
-    </div>
-            
+            <button onClick={onClick} type={type} className={className?className:'btn'} role="button">{text}</button>
     </>
 }
