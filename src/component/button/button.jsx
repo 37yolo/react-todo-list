42004738871cfeab7a1504ac8ad504e341bbb98c
37yolo <leon.vvvvvvv@gmail.com>
2024-08-06 09:31:21 +0700

    fix jittering button

--- src/component/button/button.jsx
+++ src/component/button/button.jsx
@@ -2,6 +2,9 @@ import './style.css'
 export default function Button({type , text, className, onClick}) {
 
     return<>
-            <button onClick={onClick} type={type} className={className?className:'btn'} role="button">{text}</button>
+    <div className='paddingButton'>
+        <button onClick={onClick} type={type} className={className?className:'btn'} role="button">{text}</button>
+    </div>
+            
     </>
 }
